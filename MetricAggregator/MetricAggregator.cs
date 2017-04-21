b02cbd1271505d531eff61a210e3119a3e0ed85b
Jordan Appleson <jordan@harksys.com>
2017-04-21 16:58:23 +0100

    Initial commit

---
+++ MetricAggregator/MetricAggregator.cs
@@ -0,0 +1,110 @@
+﻿// <copyright file="MetricAggregator.cs" company="HARK">
+// Copyright (c) HARK. All rights reserved.
+// </copyright>
+
+namespace MetricAggregator
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    using Microsoft.ApplicationInsights;
+    using Microsoft.ApplicationInsights.DataContracts;
+
+    public class MetricAggregator : IDisposable
+    {
+        /// <summary>
+        /// Aggrecation Interval in Seconds.
+        /// </summary>
+        private const int IntervalSeconds = 60;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MetricAggregator"/> class.
+        /// </summary>
+        /// <param name="name">The Metric Name.</param>
+        /// <param name="useAverage">A value indicating whether the aggregator should use the average of the values.</param>
+        public MetricAggregator(string name, bool useAverage = false)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentNullException(nameof(name));
+            }
+
+            this.Name = name;
+
+            this.UseAverage = useAverage;
+
+            Task.Factory.StartNew(async () =>
+            {
+                while (this.Running)
+                {
+                    await Task.Delay(TimeSpan.FromSeconds(IntervalSeconds));
+
+                    var metric = this.Aggregate();
+                    if (metric == null)
+                    {
+                        continue;
+                    }
+
+                    this.TelemetryClient.TrackMetric(metric);
+                }
+            }, TaskCreationOptions.LongRunning);
+        }
+
+        private TelemetryClient TelemetryClient { get; } = new TelemetryClient();
+
+        private List<double> Telemetry { get; } = new List<double>();
+
+        private bool Running { get; set; } = true;
+
+        private string Name { get; }
+
+        private bool UseAverage { get; }
+
+        public void Dispose()
+        {
+            this.Running = false;
+        }
+
+        public void TrackMetric(double value)
+        {
+            lock (this)
+            {
+                this.Telemetry.Add(value);
+            }
+        }
+
+        private MetricTelemetry Aggregate()
+        {
+            lock (this)
+            {
+                if (!this.Telemetry.Any())
+                {
+                    return null;
+                }
+
+                var mean = this.Telemetry.Average();
+                var variance = this.Telemetry.Sum(v => Math.Pow(v - mean, 2)) / this.Telemetry.Count;
+
+                var metric = new MetricTelemetry
+                {
+                    Timestamp = DateTime.UtcNow,
+                    Name = this.Name,
+                    Count = this.Telemetry.Count,
+                    Max = this.Telemetry.Max(),
+                    Min = this.Telemetry.Min(),
+                    StandardDeviation = Math.Sqrt(variance),
+                    Sum =
+                        this.UseAverage
+                            ? mean
+                            : this.Telemetry.Count
+                };
+
+                this.Telemetry.Clear();
+
+                return metric;
+            }
+        }
+    }
+}
---
+++ MetricAggregator/MetricAggregatorFactory.cs
@@ -0,0 +1,13 @@
+﻿namespace MetricAggregator
+{
+    public static class MetricAggregatorFactory
+    {
+        /// <summary>
+        /// Create Metric Aggregator.
+        /// </summary>
+        /// <param name="name">The Metric Name.</param>
+        /// <param name="useAverage">A value that indicates whether the values should be averaged when sent.</param>
+        /// <returns>The <see cref="MetricAggregator"/>.</returns>
+        public static MetricAggregator Create(string name, bool useAverage = false) => new MetricAggregator(name, useAverage);
+    }
+}
